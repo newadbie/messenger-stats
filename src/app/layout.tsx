c0b410d6c52ced6831022e2080e07b0b4495a93a
Adrian Bielec <bielecadriandeveloper@gmail.com>
2023-06-20 20:05:53 +0200

    clean

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+export default function RootLayout({ children }: Layout) {
+  return (
+    <html lang="pl" suppressHydrationWarning>
+      <body className="relative bg-gray-50 antialiased dark:bg-gray-900">
+        <div className="flex min-h-screen flex-grow flex-col">{children}</div>
+      </body>
+    </html>
+  );
+}
