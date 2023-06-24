cbf0e01f05ef8fcbe4bb9e1451c102ebca7e9f2a
Adrian <bielecadriandeveloper@gmail.com>
2023-06-24 11:26:22 +0200

    meta + schema improvements

--- src/app/rejestracja/page.tsx
+++ src/app/rejestracja/page.tsx
@@ -1,4 +1,10 @@
+import { type Metadata } from "next";
+
 import RegisterForm from "./RegisterForm";
+
+export const metadata: Metadata = {
+  title: "Rejestracja",
+};
 
 export default function Page() {
   return <RegisterForm />;
--- src/schemas/register.ts
+++ src/schemas/register.ts
@@ -2,10 +2,14 @@ import z from "zod";
 
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/;
 
-export const registerSchema = z.object({
-  email: z.string().email(),
-  password: z.string().regex(passwordRegex),
-  passwordConfirmation: z.string().min(10).max(100),
-});
+export const registerSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().regex(passwordRegex),
+    passwordConfirmation: z.string().min(10).max(100),
+  })
+  .refine((data) => data.password === data.passwordConfirmation, {
+    message: "Passwords do not match",
+  });
 
 export type RegisterSchema = z.infer<typeof registerSchema>;
