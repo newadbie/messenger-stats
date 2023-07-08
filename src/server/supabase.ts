c243b1a4be795afdc116b28575e69b78beda178c
Adrian <bielecadriandeveloper@gmail.com>
2023-07-08 13:19:49 +0200

    no persist

--- src/server/supabase.ts
+++ src/server/supabase.ts
@@ -6,6 +6,8 @@ const globalForPrisma = globalThis as unknown as {
   supabase: SupabaseClient | undefined;
 };
 
-export const supabase = globalForPrisma.supabase ?? createClient(env.SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_SECRET);
+export const supabase =
+  globalForPrisma.supabase ??
+  createClient(env.SUPABASE_URL, env.SUPABASE_SERVICE_ROLE_SECRET, { auth: { persistSession: false } });
 
 if (env.NODE_ENV !== 'production') globalForPrisma.supabase = supabase;
